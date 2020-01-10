0429afb720dfac05b57f50222e669cbf16fb71de
Valenlyn Chua <vchua@palo-it.com>
2020-01-10 10:39:22 +0800

    Change content

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Nav from './nav/nav.js';
-import Work from './work/work.js';
-import About from './about/about.js';
-import Contact from './contact/contact.js';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Message from "./message/message";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Nav />
-        <Route exact path="/" component={Work} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-      </div>
+      <Route exact path="/" component={Message} />
     </Router>
   );
 }
---
+++ src/message/message.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Message = () => (
+  <div className="message">
+    <p>There's nothing to see here.</p>
+  </div>
+);
+
+export default Message;
